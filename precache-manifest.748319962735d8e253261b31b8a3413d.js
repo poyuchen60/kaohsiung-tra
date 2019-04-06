58aa27fc7f2e14139041fa8ddcf2aece002eebb7
Poyu Chen <23rdstride@gmail.com>
2019-04-06 16:26:10 +0800

    Updates

---
+++ precache-manifest.748319962735d8e253261b31b8a3413d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf6bb655e41e7ef0eaf5",
+    "url": "/kaohsiung-tra/static/js/runtime~main.a1e10eac.js"
+  },
+  {
+    "revision": "90560655b95839fd40de",
+    "url": "/kaohsiung-tra/static/js/main.27bd3e42.chunk.js"
+  },
+  {
+    "revision": "1a94a735b13fda4118d5",
+    "url": "/kaohsiung-tra/static/js/2.38959f1a.chunk.js"
+  },
+  {
+    "revision": "90560655b95839fd40de",
+    "url": "/kaohsiung-tra/static/css/main.3f6ba5e9.chunk.css"
+  },
+  {
+    "revision": "2556b4df9259cbe1c2afc88e98d03c63",
+    "url": "/kaohsiung-tra/index.html"
+  }
+];

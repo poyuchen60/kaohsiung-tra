e2f70c07c7e30a48e176e502f7c7ffbedac9946c
Poyu Chen <23rdstride@gmail.com>
2019-04-11 07:05:18 +0800

    Updates

---
+++ precache-manifest.d24cb33960227a41063388693d943885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf6bb655e41e7ef0eaf5",
+    "url": "/kaohsiung-tra/static/js/runtime~main.a1e10eac.js"
+  },
+  {
+    "revision": "438202696d6e26d0b0f5",
+    "url": "/kaohsiung-tra/static/js/main.10465707.chunk.js"
+  },
+  {
+    "revision": "3c079ba22f4d9ff9f3d6",
+    "url": "/kaohsiung-tra/static/js/2.724892ae.chunk.js"
+  },
+  {
+    "revision": "438202696d6e26d0b0f5",
+    "url": "/kaohsiung-tra/static/css/main.8caab11f.chunk.css"
+  },
+  {
+    "revision": "73ea49d4b7f677f74128aebe11763d5e",
+    "url": "/kaohsiung-tra/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kaohsiung-tra/precache-manifest.948358595036eee70bfc49d6b61625e2.js"
+  "/kaohsiung-tra/precache-manifest.d24cb33960227a41063388693d943885.js"
 );
 
 workbox.clientsClaim();

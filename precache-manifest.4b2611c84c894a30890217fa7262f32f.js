c1cbc80d210265234f48060a88b6c83086931e01
Poyu Chen <23rdstride@gmail.com>
2019-04-11 07:08:02 +0800

    Updates

--- precache-manifest.4b2611c84c894a30890217fa7262f32f.js
+++ precache-manifest.4b2611c84c894a30890217fa7262f32f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/kaohsiung-tra/static/js/runtime~main.a1e10eac.js"
   },
   {
-    "revision": "438202696d6e26d0b0f5",
-    "url": "/kaohsiung-tra/static/js/main.10465707.chunk.js"
+    "revision": "ef55a0cc649de969a228",
+    "url": "/kaohsiung-tra/static/js/main.160499ea.chunk.js"
   },
   {
     "revision": "3c079ba22f4d9ff9f3d6",
     "url": "/kaohsiung-tra/static/js/2.724892ae.chunk.js"
   },
   {
-    "revision": "438202696d6e26d0b0f5",
+    "revision": "ef55a0cc649de969a228",
     "url": "/kaohsiung-tra/static/css/main.8caab11f.chunk.css"
   },
   {
-    "revision": "73ea49d4b7f677f74128aebe11763d5e",
+    "revision": "57f3451818c49bb1d7521d302592b945",
     "url": "/kaohsiung-tra/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kaohsiung-tra/precache-manifest.d24cb33960227a41063388693d943885.js"
+  "/kaohsiung-tra/precache-manifest.4b2611c84c894a30890217fa7262f32f.js"
 );
 
 workbox.clientsClaim();
